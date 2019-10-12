2104431e14ba2a602c7280b3e4a9a527764926b2
fouche <tetrapharmakon@gmail.com>
2019-10-12 19:17:31 +0200

    std

---
+++ contact.md
@@ -0,0 +1,17 @@
+---
+title: Reference people
+---
+
+# Program committee:
+- Ivan Di Liberti (Masaryk University, Brno)
+
+- Andrea Montoli (Università degli studi di Milano)
+
+- Alessio Santamaria (Queen Mary, University of London)
+
+# Local committee:
+- Cipriano Jr. Cioffo (Università degli studi di Milano)
+
+- Davide Di Micco (Università degli studi di Milano)
+
+- Andrea Montoli (Università degli studi di Milano)
---
+++ site.hs
@@ -0,0 +1,49 @@
+--------------------------------------------------------------------------------
+{-# LANGUAGE OverloadedStrings #-}
+import           Data.Monoid (mappend)
+import           Hakyll
+
+
+--------------------------------------------------------------------------------
+main :: IO ()
+main = hakyll $ do
+    match "images/*" $ do
+        route   idRoute
+        compile copyFileCompiler
+
+    match "css/*" $ do
+        route   idRoute
+        compile compressCssCompiler
+
+    match (fromList [ "programme.md"
+                    , "contact.md"
+                    , "registration.md"
+                    , "participants.md"
+                    ]) $ do
+        route   $ setExtension "html"
+        compile $ pandocCompiler
+            >>= loadAndApplyTemplate "templates/default.html" defaultContext
+            >>= relativizeUrls
+
+    match "index.html" $ do
+        route idRoute
+        compile $ do
+            -- posts <- recentFirst =<< loadAll "posts/*"
+            let indexCtx =
+                    -- listField "posts" postCtx (return posts) `mappend`
+                    constField "title" "ItaCa 2019 first workshop"          `mappend`
+                    defaultContext
+
+            getResourceBody
+                >>= applyAsTemplate indexCtx
+                >>= loadAndApplyTemplate "templates/default.html" indexCtx
+                >>= relativizeUrls
+
+    match "templates/*" $ compile templateBodyCompiler
+
+
+--------------------------------------------------------------------------------
+postCtx :: Context String
+postCtx =
+    dateField "date" "%B %e, %Y" `mappend`
+    defaultContext
