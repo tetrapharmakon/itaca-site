e8447d51b4caa0a348f2b973228467200c4a29b8
fouche <tetrapharmakon@gmail.com>
2019-10-16 19:16:21 +0200

    Comments to site.hs for DDM

--- site.hs
+++ site.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
-import Data.Monoid (mappend)
-import Hakyll
+import           Data.Monoid (mappend)
+import           Hakyll
+-- ^ non toccare questa parte: il pragma e gli import servono!
 
 main :: IO ()
 main = hakyll $ do
@@ -12,15 +13,24 @@ main = hakyll $ do
     route   idRoute
     compile compressCssCompiler
 
-  match (fromList [ "contact.md"
+  match (fromList [ "pages/contact.md"
                   , "index.md"
-                  , "participants.md"
-                  , "programme.md"
-                  , "registration.md"
+                  , "pages/participants.md"
+                  , "pages/programme.md"
+                  , "pages/registration.md"
                   ]) $ do
+    -- le opzioni che sono fissate dalla concatenazione monadica che segue
+    -- sono applicate a tutti gli item della lista sopra ^
     route   $ setExtension "html"
     compile $ pandocCompiler
       >>= loadAndApplyTemplate "templates/default.html" defaultContext
+      -- questa monade applica il template di default alle pagine, definito nel
+      -- file templates/default.html.
       >>= relativizeUrls
-
+      {- pandoc genera pagine html a partire dal markdown, e questa monade
+         gli insegna a creare hyperlink relativi; la monade traversa l'HTML
+         "and changes absolute URLs (e.g. /posts/foo.markdown into relative
+         ones: ../posts/foo.markdown). This is extremely useful if you want
+         to deploy your site in a subdirectory.""
+      -}
   match "templates/*" $ compile templateBodyCompiler
